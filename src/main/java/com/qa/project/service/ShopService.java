581fa9637aabd462f79cb56df9a0be9b94ce986e
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-28 14:56:40 +0100

    add shop service class update for DTO integration

--- src/main/java/com/qa/project/service/ShopService.java
+++ src/main/java/com/qa/project/service/ShopService.java
@@ -1,45 +1,54 @@
 package com.qa.project.service;
 
 import com.qa.project.domain.Shop;
+import com.qa.project.dto.ShopDTO;
 import com.qa.project.exceptions.ShopNotFoundException;
 import com.qa.project.repo.ShopRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ShopService {
     private final ShopRepository shopRepository;
+    private final ModelMapper mapper;
+
     @Autowired
-    public ShopService(ShopRepository shopRepository) {
+    public ShopService(ShopRepository shopRepository, ModelMapper mapper) {
         this.shopRepository = shopRepository;
+        this.mapper = mapper;
     }
 
-    public List<Shop> viewShops() {
-        //grabs all notes from table
-        return this.shopRepository.findAll();
+    private ShopDTO mapToDTO(Shop shop){
+        return this.mapper.map(shop, ShopDTO.class);
     }
-    public Shop create(Shop shop) {
-        return this.shopRepository.save(shop);
-    }
-    public Shop findShopById(Long id) {
-        return this.shopRepository.findById(id).orElseThrow(ShopNotFoundException::new);
-    }
-    public Shop update(Long id, Shop shop) {
 
-        Shop updateShop = findShopById(id);
-        updateShop.setShopName(shop.getShopName());
-
-        return this.shopRepository.save(updateShop);
+    public List<ShopDTO> getShops(){
+        return this.shopRepository.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
-    public Boolean delete(Long id) {
-        //checks if item exists by given id
-        if(!this.shopRepository.existsById(id)) {
+
+    public ShopDTO createShop(Shop shop){
+        return this.mapToDTO(this.shopRepository.save(shop));
+    }
+
+    public ShopDTO findShopById(Long id){
+        return this.mapToDTO(this.shopRepository.findById(id).orElseThrow(ShopNotFoundException::new));
+    }
+
+    public ShopDTO updateShop(Long id, Shop shop){
+        Shop update = this.shopRepository.findById(id).orElseThrow(ShopNotFoundException::new);
+        update.setShopName(shop.getShopName());
+        return this.mapToDTO(this.shopRepository.save(update));
+    }
+
+    public Boolean deleteShop(Long id){
+        if(!this.shopRepository.existsById(id)){
             throw new ShopNotFoundException();
         }
         this.shopRepository.deleteById(id);
         return this.shopRepository.existsById(id);
-
     }
 
 }
