b877e73320345a745ddf36d649c532d3d985a7c1
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-31 19:24:05 +0100

    add shopDTO tests

--- src/test/java/com/qa/project/dto/ShopDTOTest.java
+++ src/test/java/com/qa/project/dto/ShopDTOTest.java
@@ -1,6 +1,21 @@
 package com.qa.project.dto;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class ShopDTOTest {
+    @Test
+    public void testShopDTO() {
+        ShopDTO test1 = new ShopDTO();
+        ShopDTO test2 = new ShopDTO("KFC");
+
+        test1.setId(1);
+        test2.setId(2);
+        assertEquals(test1.getId(), 1);
+        test1.setShopName("shop");
+        assertEquals(test1.getShopName(), "shop");
+
+
+    }
 
 }
